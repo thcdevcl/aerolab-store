03d10cd08c5c49270d17fe3bf1fb2ff749f61fad
Diego Robles <diego@hemployeed.com>
2020-02-24 01:50:57 -0300

    fix(product-card):stringify request body

--- src/ui/components/cards/ProductCard.jsx
+++ src/ui/components/cards/ProductCard.jsx
@@ -76,7 +76,7 @@ function ProductContextProvider(props) {
         "Content-Type": "application/json",
         Authorization: `Bearer ${process.env.REACT_APP_AEROLAB_API_KEY}`
       },
-      body: { productId: _id }
+      body: JSON.stringify(`{"productId": "${_id}"}`)
     })
       .then(result =>
         result.json().then(message => {
